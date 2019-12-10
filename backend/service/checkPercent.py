06b48f763117c20341334bb8451e20058b65467f
andradatapuc <andrada.tapuc@gmail.com>
2019-12-10 19:42:50 +0200

    Check the percentage of correct answers

--- backend/service/checkPercent.py
+++ backend/service/checkPercent.py
@@ -1,7 +1,12 @@
-
 from Features import is_definition
 
+trueCheck = 0
+falseCheck = 0
+totalSent = 0
+
+
 def checkPercent(file_read):
+    global trueCheck, falseCheck, totalSent
     with open(file_read, 'r') as ins:
         line_list = []
         for line in ins:
@@ -12,22 +17,18 @@ def checkPercent(file_read):
         for l in range(len(line_list)):
             sentences_list.append(line_list[l][0:-2])
             val_list.append(line_list[l][-2])
-
-    print(val_list)
-    trueCheck = 0
-    falseCheck = 0
+    totalSent = totalSent + len(sentences_list)
 
     for i in range(len(sentences_list)):
-        print(is_definition(sentences_list[i]))
         if (is_definition(sentences_list[i]) == True) and (val_list[i] == '1'):
             trueCheck = trueCheck + 1
         elif (is_definition(sentences_list[i]) == False) and (val_list[i] == '0'):
             trueCheck = trueCheck + 1
         else:
             falseCheck = falseCheck + 1
-    print("True check:", trueCheck)
-    print("False check:", falseCheck)
+
+    percent = trueCheck / totalSent * 100
+    print("The percentage of correct answers:", percent, '%')
 
 
-
-checkPercent("../../data/deft_files/dev/t1_biology_0_404.verif")
+# checkPercent("../../data/deft_files/dev/t1_biology_0_404.verif")
